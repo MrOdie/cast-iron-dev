e165eef71b4a5db56f94d42bad4d3f831aa9e218
Sam O'Donnell (Oh-Dee) <sodonnell5214@gmail.com>
2023-08-20 15:43:00 -0400

    Starting ground / got rid of tailwind bc ewwwww. easier to use own styleguide than learn tailwind and then find out that I can't use a file bc I wont pay  for it.

---
+++ src/app/components/navigation.tsx
@@ -0,0 +1,15 @@
+
+const navigation = [
+    { name: 'Dashboard', href: '#', current: true },
+    { name: 'Team', href: '#', current: false },
+    { name: 'Projects', href: '#', current: false },
+    { name: 'Calendar', href: '#', current: false },
+]
+
+const Navigation = () => {
+    return (
+        "Hello world"
+    )
+}
+
+export default Navigation;
