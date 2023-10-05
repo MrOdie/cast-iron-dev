0e23395ba81521c52ab9fec9d9de1b4f753a48d8
Sam O'Donnell (Oh-Dee) <sodonnell5214@gmail.com>
2023-10-04 21:23:02 -0400

    Cleaning up to make it a decent example for Portfolio site.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,4 @@
 // import React Componenets
-import Navbar from "@/components/globals/navigation/navigation";
-
 // import Styles
 import sassy from "./layout.module.scss";
 import "../sass/index.scss";
@@ -9,19 +7,18 @@ import "../sass/index.scss";
 import "../fonts/typography.css";
 
 export const metadata = {
-  title: 'React Playground',
-  description: 'Knocking the rust off of my front end experience.',
+    title: 'React Playground',
+    description: 'Knocking the rust off of my front end experience.',
 }
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={sassy.body}>
-        <Navbar></Navbar>
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body className={sassy.body}>
         <main className={sassy.main}>
-          {children}
+            {children}
         </main>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,6 @@ import sassy from "./page.module.scss";
 const Home = () => {
     return (
         <section className={sassy.section}>
-            <article className={sassy.article_base}>
-                <h1>This is the main content.</h1>
-            </article>
-
             <article className={sassy.article_alt}>
                 <TodoApp></TodoApp>
             </article>
