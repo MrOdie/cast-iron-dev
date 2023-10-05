ae6a873341c8d680e646c9240ad17ea7a93ef04b
Sam O'Donnell (Oh-Dee) <sodonnell5214@gmail.com>
2023-10-04 21:18:02 -0400

    Decided to rework projects page.

--- src/app/assets/types/projects/project.ts
+++ src/app/assets/types/projects/project.ts
@@ -3,16 +3,12 @@ export class Project {
     name: string;
     link: string;
     description: string;
-    age: number;
-    image: string;
 
 
-    constructor(id: number, name: string, link: string, description: string, age: number, image: string) {
+    constructor(id: number, name: string, link: string, description: string) {
         this.id = id;
         this.name = name;
         this.link = link;
         this.description = description;
-        this.age = age;
-        this.image = image;
     }
 }
--- src/app/assets/types/projects/projectList.ts
+++ src/app/assets/types/projects/projectList.ts
@@ -3,43 +3,45 @@ import {Project} from "@/app/assets/types/projects/project";
 const ProjectList: Project[] = [
     {
         id: 0,
-        name: "Alabama Tribute Page",
-        link: "https://github.com/cast-iron-developer/Alabama-Tribute-Page",
-        description: "This is the first Vanilla HTML/CSS site that I ever created.",
-        age: 24,
-        image: "https://images.pexels.com/photos/1921326/pexels-photo-1921326.jpeg"
+        name: "Simple Java API",
+        link: "https://github.com/cast-iron-developer/simple_java_api",
+        description: "",
     },
     {
         id: 1,
-        name: "Tetris",
-        link: "https://github.com/cast-iron-developer/tetris",
-        description: "A Vanilla JS game of Tetris, using a Canvas element.",
-        age: 15,
-        image: "https://images.pexels.com/photos/1921326/pexels-photo-1921326.jpeg"
+        name: "React Todo",
+        link: "https://github.com/cast-iron-developer/simple_java_api",
+        description: "",
     },
     {
         id: 2,
-        name: "Image Portfolio",
-        link: "https://github.com/cast-iron-developer/Image-portfolio",
-        description: "A simple static landing page for an image portfolio.",
-        age: 3,
-        image: "https://images.pexels.com/photos/1921326/pexels-photo-1921326.jpeg"
+        name: "Java Password Generator",
+        link: "https://github.com/cast-iron-developer/java-password-genertor",
+        description: "",
     },
     {
         id: 3,
-        name: "Pong",
-        link: "https://github.com/cast-iron-developer/Pong",
-        description: "A Vanilla JS game of Pong.",
-        age: 25,
-        image: "https://images.pexels.com/photos/1921326/pexels-photo-1921326.jpeg"
+        name: "Alabama Tribute Page",
+        link: "https://github.com/cast-iron-developer/Alabama-Tribute-Page",
+        description: "This is the first Vanilla HTML/CSS site that I ever created.",
     },
     {
         id: 4,
-        name: "Fifth Project",
-        link: "https://github.com/cast-iron-developer/Alabama-Tribute-Page",
-        description: "",
-        age: 6,
-        image: "https://images.pexels.com/photos/1921326/pexels-photo-1921326.jpeg"
+        name: "Tetris",
+        link: "https://github.com/cast-iron-developer/tetris",
+        description: "A Vanilla JS game of Tetris, using a Canvas element.",
+    },
+    {
+        id: 5,
+        name: "Image Portfolio",
+        link: "https://github.com/cast-iron-developer/Image-portfolio",
+        description: "A simple static landing page for an image portfolio.",
+    },
+    {
+        id: 6,
+        name: "Pong",
+        link: "https://github.com/cast-iron-developer/Pong",
+        description: ""
     }
 ];
 
