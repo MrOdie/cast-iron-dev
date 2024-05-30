66c2adf3f9087e61e0bec4902ec38783b0c40734
Sam O'Donnell (Oh-Dee) <sodonnell5214@gmail.com>
2024-05-29 20:42:53 -0400

    Initial Commit.

--- src/app/projects/layout.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/app/assets/styles/modules/projectlayout.module.scss";
-
-const ProjectLayout = ({children,}: { children: React.ReactNode }) => {
-    return (
-        <main className={styles.main}>{children}</main>
-    )
-}
-
-export default ProjectLayout;
